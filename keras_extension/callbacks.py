096bbb0d2ea7cea1db1834dd5a4f1058b9bf83c7
liuzhehao <liuzhehao@zaonline.com>
2019-01-03 17:13:48 +0800

    Add earlystopping for AUCEpochEvaluator

--- keras_extension/callbacks.py
+++ keras_extension/callbacks.py
@@ -1,4 +1,5 @@
 from keras.callbacks import Callback
+import numpy as np
 from sklearn.metrics import roc_auc_score
 
 
@@ -6,15 +7,32 @@ class BaseEpochEvaluator(Callback):
     """ Base class for performing evaluation after every epoch."""
     eval_name = ''
     eval_f = None
+    mode = 'min'
 
-    def __init__(self, training_data=None, validation_data=None):
+    def __init__(self, training_data=None, validation_data=None,
+                 earlystopping=False, patience=np.inf):
         super().__init__()
         self.train = training_data
         self.val = validation_data
         self.metrics = list()
 
+        if earlystopping and validation_data is None:
+            raise ValueError('Validation data is needed to earlystopping to work.')
+        self.earlystopping = earlystopping
+        self.patience = patience
+        self.stopped_epoch = 0
+        self.wait = 0
+        self.best = None
+
+        if self.mode == 'min':
+            self.monitor_op = np.less
+        else:
+            self.monitor_op = np.greater
+
     def on_train_begin(self, logs=None):
-        pass
+        self.wait = 0
+        self.stopped_epoch = 0
+        self.best = np.inf if self.mode == 'min' else -np.inf
 
     def on_train_end(self, logs=None):
         pass
@@ -31,7 +49,17 @@ class BaseEpochEvaluator(Callback):
         if val_metric is not None:
             stats['val_{}'.format(self.eval_name)] = val_metric
         self.metrics.append(stats)
-        print(' '.join([train_str, val_str]))
+        print('\n'.join([train_str, val_str]))
+
+        # check for early stopping
+        if self.monitor_op(val_metric, self.best):
+            self.best = val_metric
+            self.wait = 0
+        else:
+            self.wait += 1
+            if self.wait >= self.patience:
+                self.stopped_epoch = epoch
+                self.model.stop_training = True
 
     def _eval(self, data=None, mode='train'):
         if data:
@@ -47,4 +75,4 @@ class AUCEpochEvaluator(BaseEpochEvaluator):
     """ Callback for calculating AUC after every epoch """
     eval_name = 'auc'
     eval_f = roc_auc_score
-
+    mode = 'max'
