7f6ea335e48b080c6df214540a6514ce375d4495
liuzhehao <liuzhehao@zaonline.com>
2019-02-13 17:24:31 +0800

    [FET] add calculate_relationship

--- metrics/similarity.py
+++ metrics/similarity.py
@@ -1,18 +1,33 @@
 def jarccard_index(A, B, ignore_na=True):
-	""" Calculate the jarccard index (aka jaccard similarity). 
-	:param ignore_na: Whether to filter out the missing value before calculation
-	"""
-	def to_nonmissing_set(X):
-		from sklearn.utils import is_scalar_nan
-		return set(filter(lambda x: not is_scalar_nan(x), X))
-	if ignore_na:
-		A, B = to_nonmissing_set(A), to_nonmissing_set(B)
-	else:
-		A, B = set(A), set(B)
-	return len(A & B) / len(A | B)
+    """ Calculate the jarccard index (aka jaccard similarity).
+    :param ignore_na: Whether to filter out the missing value before calculation
+    """
+    def to_nonmissing_set(X):
+        from sklearn.utils import is_scalar_nan
+        return set(filter(lambda x: not is_scalar_nan(x), X))
+    if ignore_na:
+        A, B = to_nonmissing_set(A), to_nonmissing_set(B)
+    else:
+        A, B = set(A), set(B)
+    return len(A & B) / len(A | B)
 
 
 # alias
 jaccard_similarity = jarccard_index
 
+
+def calculate_relationship(x, y):
+    """ Determine if y is positive|negative|unrelated with x"""
+    import statsmodels.api as sm
+
+    x = sm.add_constant(x)
+    model = sm.OLS(y, x, missing='drop')
+    model_result = model.fit()
+    coef = model_result.params[1]
+    if coef > 0:
+        return 'postive'
+    elif coef == 0:
+        return 'unrelated'
+    else:
+        return 'negative'
 
