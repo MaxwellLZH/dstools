82417a78fb6b1eeb881316f56fed85a3caa16729
Max Liu <zl2480@columbia.edu>
2019-02-16 15:04:50 +0800

    [FET] start on save_to

--- utils/io.py
+++ utils/io.py
@@ -1,5 +1,6 @@
 import contextlib
 import pandas as pd
+import functools
 
 
 @contextlib.contextmanager
@@ -44,3 +45,68 @@ def read_sheets(path, **kwargs):
     """ Read all the sheets in an excel file and concatenate them row-wise """
     sheets = pd.read_excel(path, sheet_name=None)
     return pd.concat(sheets.values(), axis=0)
+
+
+
+def save_to(path, n_obj=1, mode='auto'):
+    def decorator(f):
+        import os
+        
+        f_name = f.__name__
+        dir_name = os.path.dirname(path)
+        file_name = os.path.basename(path).split('.')[0]
+
+
+        def save_file(obj, out_path, mode, suffix=''):
+            _save_mode = {
+                pd.DataFrame: 'csv',
+                str: 'txt',
+                list: 'txt',
+                tuple: 'txt',
+                dict: 'txt',
+            }
+
+            if mode == 'auto':
+                mode = _save_mode.get(type(obj), 'pkl')
+
+            out_path = out_path + suffix + '.' + mode
+            if os.path.exists(out_path):
+                raise ValueError('File {} has already exists'.format(outpath))
+
+            if mode == 'csv':
+                if isinstance(obj, pd.DataFrame):
+                    obj.to_csv(out_path)
+                else:
+                    raise ValueError('Only support saving Pandas DataFrame as csv file.')
+
+
+        def save(*args, **kwargs):
+            result = f(*args, **kwargs)
+
+            out_path = os.path.join(dir_name, path_name)
+            if n_obj == 1:
+                save_file(result, out_path, mode)
+            else:
+                # just in case an generator is returned
+                result = list(result)
+                n_result = len(result)
+                if n_result != n_obj:
+                    raise ValueError('The number of outputs {} does not match with expected '
+                            'number of output {}'.format(n_result, n_obj))
+
+                for i, obj in enumerate(result):
+                    suffix = '_' + str(i)
+                    save_file(obj, out_path, mode, suffix)
+
+            return result
+
+        return save
+    return decorator
+
+
+
+
+
+
+
+
