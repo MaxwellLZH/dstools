d9ea2f9085f28c5457a800b77b25e092530e4b91
liuzhehao <liuzhehao@zaonline.com>
2019-04-08 14:48:18 +0800

    Add PSI calculation

--- metrics/model_evaluation.py
+++ metrics/model_evaluation.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def ks_score(y_true, y_score):
     """ Calculating the Kolmogorov-Smirnov score"""
     from sklearn import metrics
@@ -34,3 +37,41 @@ def lift_table(prediction, label, bins=10, mode='equal_width'):
     lift_table['pct_sample'] = lift_table['n_sample'] / lift_table['n_sample'].sum()
     return lift_table
 
+
+def psi(expected_array, actual_array, buckets=20, buckettype='bins'):
+    """ Calculate the PSI for a single variable
+    Args:
+       expected_array: numpy array of original values
+       actual_array: numpy array of new values, same size as expected
+       buckets: number of percentile ranges to bucket the values into
+       buckettype: available choices: ['bins', 'quantiles']
+    Returns:
+       psi_value: calculated PSI value
+    """
+    def scale_range(input, min, max):
+        input += -(np.min(input))
+        input /= np.max(input) / (max - min)
+        input += min
+        return input
+
+    breakpoints = np.arange(0, buckets + 1) / (buckets) * 100
+    if buckettype == 'bins':
+        breakpoints = scale_range(breakpoints, np.min(expected_array), np.max(expected_array))
+    elif buckettype == 'quantiles':
+        breakpoints = np.stack([np.percentile(expected_array, b) for b in breakpoints])
+
+    expected_percents = np.histogram(expected_array, breakpoints)[0] / len(expected_array)
+    actual_percents = np.histogram(actual_array, breakpoints)[0] / len(actual_array)
+
+    def sub_psi(e_perc, a_perc):
+        """ Calculate the actual PSI value from comparing the values.
+           Update the actual value to a very small number if equal to zero """
+        if a_perc == 0:
+            a_perc = 0.0001
+        if e_perc == 0:
+            e_perc = 0.0001
+        value = (e_perc - a_perc) * np.log(e_perc / a_perc)
+        return value
+
+    psi_value = np.sum(sub_psi(expected_percents[i], actual_percents[i]) for i in range(0, len(expected_percents)))
+    return psi_value
