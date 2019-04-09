1fb4227a27eaac0914bee2d797768515a7b9e83b
liuzhehao <liuzhehao@zaonline.com>
2019-04-09 17:19:01 +0800

    [Fet] write dictionary of dataframes to excel file

--- utils/io.py
+++ utils/io.py
@@ -47,62 +47,12 @@ def read_sheets(path, **kwargs):
     return pd.concat(sheets.values(), axis=0)
 
 
-
-def save_to(path, n_obj=1, mode='auto'):
-    def decorator(f):
-        import os
-        
-        f_name = f.__name__
-        dir_name = os.path.dirname(path)
-        file_name = os.path.basename(path).split('.')[0]
-
-
-        def save_file(obj, out_path, mode, suffix=''):
-            _save_mode = {
-                pd.DataFrame: 'csv',
-                str: 'txt',
-                list: 'txt',
-                tuple: 'txt',
-                dict: 'txt',
-            }
-
-            if mode == 'auto':
-                mode = _save_mode.get(type(obj), 'pkl')
-
-            out_path = out_path + suffix + '.' + mode
-            if os.path.exists(out_path):
-                raise ValueError('File {} has already exists'.format(outpath))
-
-            if mode == 'csv':
-                if isinstance(obj, pd.DataFrame):
-                    obj.to_csv(out_path)
-                else:
-                    raise ValueError('Only support saving Pandas DataFrame as csv file.')
-
-
-        def save(*args, **kwargs):
-            result = f(*args, **kwargs)
-
-            out_path = os.path.join(dir_name, path_name)
-            if n_obj == 1:
-                save_file(result, out_path, mode)
-            else:
-                # just in case an generator is returned
-                result = list(result)
-                n_result = len(result)
-                if n_result != n_obj:
-                    raise ValueError('The number of outputs {} does not match with expected '
-                            'number of output {}'.format(n_result, n_obj))
-
-                for i, obj in enumerate(result):
-                    suffix = '_' + str(i)
-                    save_file(obj, out_path, mode, suffix)
-
-            return result
-
-        return save
-    return decorator
-
+def write_dict_to_excel(d, path, **kwargs):
+    """ Save a dictionary to an Excel file with each key being the sheet name """
+    with pd.ExcelWriter(path) as writer:
+        for sheet_name, df in d.items():
+            df.to_excel(writer, sheet_name, **kwargs)
+        writer.save()
 
 
 
