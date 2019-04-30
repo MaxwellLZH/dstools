d4541b56b9f635b5b8bae666212d5ecbf0326568
liuzhehao <liuzhehao@zaonline.com>
2019-04-30 10:00:09 +0800

    Add iter_date function

--- utils/date.py
+++ utils/date.py
@@ -3,3 +3,24 @@ def today(fmt='%Y%m%d'):
     from datetime import datetime
     dt = datetime.today()
     return dt.strftime(fmt)
+
+
+def iter_date(start, end, step=1, fmt=None, include_end=True):
+    """ Iterate over days
+    :param fmt: If set to None, yield datetime object, elsewise return a formatted string
+    """
+    from dateutil.relativedelta import relativedelta
+    from dateutil import parser
+
+    if isinstance(start, str):
+        start = parser.parse(start)
+    if isinstance(end, str):
+        end = parser.parse(end)
+
+    step = relativedelta(days=step)
+    cursor = start
+    while cursor <= end:
+        yield cursor.strftime(fmt) if fmt else cursor
+        cursor += step
+    if include_end and cursor != end:
+        yield end.strftime(fmt) if fmt else cursor
